5c02637d87474a973fbb7c9985df141cac9c2c5b
Jose A. Gonzalez <jglez.dev@gmail.com>
2024-09-21 21:03:03 -0500

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  preset: "ts-jest",
+  preset: 'ts-jest',
   globals: {
-    __DEV__: true,
+    __DEV__: true
   },
   verbose: true,
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
-};
+    '^.+.tsx?$': ['ts-jest', {}]
+  }
+}
